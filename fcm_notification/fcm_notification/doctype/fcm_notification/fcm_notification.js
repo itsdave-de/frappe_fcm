5b72ab61dee5075fc84344735820e4069002cee3
Luiz Costa <luizgustavo@luizgustavo.pro.br>
2025-01-21 10:54:14 -0300

    add js for all_users

--- fcm_notification/fcm_notification/doctype/fcm_notification/fcm_notification.js
+++ fcm_notification/fcm_notification/doctype/fcm_notification/fcm_notification.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('FCM Notification', {
-	// refresh: function(frm) {
-
-	// }
+    all_users: function (frm) {
+        if (frm.doc.all_users) {
+            // clean the user field and make it read only
+            frm.set_value('user', null);
+            frm.set_df_property('user', 'read_only', 1);
+        } else {
+            // Set the user field to be editable
+            frm.set_df_property('user', 'read_only', 0);
+        }
+    }
 });
